5044b9d3059806fb64cc4282c40cdbb141467ffd
ErwinFeng <xfsy_2015@163.com>
2023-04-27 16:04:44 +0800

    设置导航页 and 初始化日期时间工具类

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,21 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '指南',
+        link: '/guide/',
+      },
+    ],
+  }),
 })
